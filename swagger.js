63f5f4aea413cf618144d065b979158e99faae46
Dazuva <159646919+Dazuva@users.noreply.github.com>
2024-12-17 20:46:24 -0600

    Actualizacion

--- swagger.js
+++ swagger.js
@@ -1,34 +1,19 @@
-const { version } = await import('eslint-plugin-prettier');
+import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
 
-const swaggerDefinition = {
-openapi: '3.0.0',
-info:{
-  title: 'Documento de la API',
-  version: '1.0.0',
-  description: 'Documentacion de la API con swagger'
-},
+export function setupSwagger(app) {
+  const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'API Documentation',
+        version: '1.0.0',
+        description: 'Documentación de la API para el manejo de áreas y empleados',
+      },
+    },
+    apis: ['./routes/*.js'], // Aquí deberías poner la ruta de tu archivo de rutas
+  };
 
-  servers:[
-
-  {
-    url: 'http://localhost:3000',
-    description: 'Servidor de Desarrollo'
-  }
-]
-
+  const specs = swaggerJsdoc(options);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 }
-
-const options = {
-  swaggerDefinition,
-  apis: ['./route/*.js']
-}
-
-const swaggerSpec = swaggerJSDoc(options)
- function setupSwagger(app){
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
- }
-
- export {setupSwagger}
-
